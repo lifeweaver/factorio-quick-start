6dbfae44b4f3a41e8298084eaadffe16533fed7e
Pascal Ludwig <pascal.ludwig@mantro.net>
2017-05-08 21:04:23 +0200

    Added 3 types of start kits and a mod setting to select one of them.
    Also added the capability to unlock technologies

--- control.lua
+++ control.lua
@@ -11,7 +11,42 @@ script.on_event(defines.events.on_player_created, function(event)
 		defines.inventory.player_player_trash
 	}
 	
-	local items = {
+	-- Setup kit presets
+	local kits = {}
+	
+	kits["small"] = {}
+	kits["small"]["quickbar"] = {
+		{1, "transport-belt"},
+		{2, "underground-belt"},
+		{3, "splitter"},
+		{4, "inserter"},
+		{5, "medium-electric-pole"},
+		{6, "deconstruction-planner"},
+		{10, "car"}
+	}
+	kits["small"]["items"] = {
+		{"steel-axe", 10},
+		{"iron-plate", 192},
+		{"copper-plate", 200},
+		{"iron-gear-wheel", 50},
+		{"transport-belt", 500},
+		{"splitter", 50},
+		{"underground-belt", 50},
+		{"burner-mining-drill", 20},
+		{"coal", 100}
+	}
+		
+	kits["medium"] = {}
+	kits["medium"]["quickbar"] = {
+		{1, "transport-belt"},
+		{2, "underground-belt"},
+		{3, "splitter"},
+		{4, "inserter"},
+		{5, "medium-electric-pole"},
+		{6, "deconstruction-planner"},
+		{10, "car"}
+	}
+	kits["medium"]["items"] = {
 		{"steel-axe", 10},
 		{"iron-plate", 592},
 		{"copper-plate", 400},
@@ -26,7 +61,7 @@ script.on_event(defines.events.on_player_created, function(event)
 		{"long-handed-inserter", 50},
 		{"steel-chest", 50},
 		{"electric-mining-drill", 50},
-		{"small-electric-pole", 200},
+		{"medium-electric-pole", 200},
 		{"boiler", 10},
 		{"steam-engine", 20},
 		{"offshore-pump", 1},
@@ -39,8 +74,7 @@ script.on_event(defines.events.on_player_created, function(event)
 		{"deconstruction-planner", 1},
 		{"power-armor", 1}
 	}
-	
-	local armorItems = {
+	kits["medium"]["armorItems"] = {
 		{"fusion-reactor-equipment"},
 		{"personal-roboport-equipment"},
 		{"personal-roboport-equipment"},
@@ -52,6 +86,78 @@ script.on_event(defines.events.on_player_created, function(event)
 		{"battery-equipment"}
 	}
 	
+	kits["big"] = {}
+	kits["big"]["quickbar"] = {
+		{1, "transport-belt"},
+		{2, "underground-belt"},
+		{3, "splitter"},
+		{4, "inserter"},
+		{5, "medium-electric-pole"},
+		{6, "deconstruction-planner"},
+		{10, "car"}
+	}
+	kits["big"]["items"] = {
+		{"power-armor", 1},
+		{"steel-axe", 10},
+		{"iron-plate", 592},
+		{"copper-plate", 400},
+		{"iron-gear-wheel", 200},
+		{"electronic-circuit", 200},
+		{"advanced-circuit", 200},
+		{"transport-belt", 1500},
+		{"underground-belt", 50},
+		{"splitter", 50},
+		{"steel-furnace", 100},
+		{"assembling-machine-2", 100},
+		{"inserter", 300},
+		{"long-handed-inserter", 50},
+		{"steel-chest", 50},
+		{"electric-mining-drill", 50},
+		{"medium-electric-pole", 400},
+		{"big-electric-pole", 100},
+		{"logistic-chest-requester", 100},
+		{"logistic-chest-passive-provider", 100},
+		{"boiler", 20},
+		{"steam-engine", 40},
+		{"offshore-pump", 10},
+		{"pipe-to-ground", 100},
+		{"pipe", 100},
+		{"chemical-plant", 20},
+		{"oil-refinery", 10}, 
+		{"car", 1},
+		{"coal", 50},
+		{"roboport", 20},
+		{"construction-robot", 50},
+		{"logistic-robot", 300},
+		{"lab", 10},
+		{"deconstruction-planner", 1}
+	}
+	kits["big"]["armorItems"] = kits["medium"]["armorItems"]
+	kits["big"]["technologies"] = {
+		{"automation"},
+		{"steel-processing"},
+		{"automation-2"},
+		{"oil-processing"},
+		{"plastics"},
+		{"advanced-electronics"},
+		{"sulfur-processing"},
+		{"battery"},
+		{"electronics"},
+		{"engine"},
+		{"electric-engine"},
+		{"flying"},
+		{"robotics"},
+		{"logistic-robotics"},
+		{"construction-robotics"},
+		{"logistic-system"}
+	}
+	
+	local kitSetting = settings.startup["quick-start-kit"].value
+	local kit = kits[kitSetting]
+	if kit == nil then
+		kit = kits["medium"]
+	end
+	
 	-- Find quickbar (usually player_quickbar, but god_quickbar in sandbox mode)
 	local quickbar = player.get_inventory(defines.inventory.player_quickbar)
 	if quickbar ~= nil and not quickbar.can_set_filter(1, "transport-belt") then
@@ -61,34 +167,41 @@ script.on_event(defines.events.on_player_created, function(event)
 	-- Setup quickbar favorites
 	if quickbar ~= nil and quickbar.can_set_filter(1, "transport-belt") ~= nil then
 		quickbar.clear()
-		quickbar.set_filter(1, "transport-belt")
-		quickbar.set_filter(2, "underground-belt")
-		quickbar.set_filter(3, "splitter")
-		quickbar.set_filter(4, "inserter")
-		quickbar.set_filter(5, "small-electric-pole")
-		quickbar.set_filter(6, "deconstruction-planner")
-		quickbar.set_filter(10, "car")
+		for k,v in pairs(kit["quickbar"]) do
+			quickbar.set_filter(v[1], v[2])
+		end
 	end
 	
 	-- Add items
-	for k,v in pairs(items) do
+	for k,v in pairs(kit["items"]) do
 		player.insert{name = v[1], count = v[2]}
 	end
 	
-	-- Find armor in one of the inventories
-	-- Usually ends up in the armor slot. But that one does not exist in sandbox mode
-	for k,v in pairs(inventories) do
-		local inventory = player.get_inventory(v)
-		if inventory ~= nil then
-			local armor = inventory.find_item_stack("power-armor")
-			if armor ~= nil then
-				-- Add items to armor grid
-				local grid = armor.grid
-				for k,v in pairs(armorItems) do
-					grid.put{name = v[1]}
+	if kit["armorItems"] ~= nil then
+		-- Find armor in one of the inventories
+		-- Usually ends up in the armor slot. But that one does not exist in sandbox mode
+		for k,v in pairs(inventories) do
+			local inventory = player.get_inventory(v)
+			if inventory ~= nil then
+				local armor = inventory.find_item_stack("power-armor")
+				if armor ~= nil then
+					-- Add items to armor grid
+					local grid = armor.grid
+					for k,v in pairs(kit["armorItems"]) do
+						grid.put{name = v[1]}
+					end
+					break
 				end
-				break
 			end
 		end
 	end
+	
+	
+	-- Unlock technologies
+	if kit["technologies"] ~= nil then
+		for k,v in pairs(kit["technologies"]) do
+			player.force.technologies[v[1]].researched = true
+		end
+	end
+	
 end)
